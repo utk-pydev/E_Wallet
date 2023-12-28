4ffc885b3804f3b45535a6fe50fb461cfe7e4ac2
utk-pydev <utkarshvashisth94@gmail.com>
2023-12-28 22:17:37 +0530

    Security Config Added in Notification Service

--- Notification-Service/src/main/java/org/example/wallet/wallet/NotificationConfig.java
+++ Notification-Service/src/main/java/org/example/wallet/wallet/NotificationConfig.java
@@ -1,7 +1,49 @@
 package org.example.wallet.wallet;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.*;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+
+import javax.mail.internet.MimeMessage;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Configuration
 public class NotificationConfig {
+
+
+    Properties getCProperties(){
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return properties;
+    }
+    @Bean
+    ConsumerFactory getConsumerFactory(){
+        return new DefaultKafkaConsumerFactory(getCProperties());
+    }
+
+    @Bean
+    SimpleMailMessage getMailMessage(){
+        return new SimpleMailMessage();
+    }
+
+    @Bean
+    JavaMailSender getMailSender(){
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.gmail.com");
+
+        return  javaMailSender;
+    }
+
 }
